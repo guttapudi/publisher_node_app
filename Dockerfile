14e70b6b4fe2066f8e78c6885deb4cf486f66963
karthikguttapudi <karthikg@sg.ibm.com>
2022-03-19 10:12:11 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM registry.access.redhat.com/ubi8/ubi
+RUN dnf install -y gcc-c++ make curl
+RUN curl -sL https://rpm.nodesource.com/setup_14.x | bash -
+RUN dnf install -y nodejs
+WORKDIR /usr/app
+COPY package.json package-lock.json ./
+COPY index.js ./
+RUN npm install
+CMD [ "npm", "start" ]
---
+++ index.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const app = express();
+
+const {PubSub} = require('@google-cloud/pubsub');
+const pubSubClient = new PubSub();
+const  topicNameOrId = 'workload-identity-test';
+
+let data = JSON.stringify({"employees":{"employee":[{"id":"1","firstName":"Tom","lastName":"Cruise"},{"id":"2","firstName":"Maria","lastName":"Sharapova"},{"id":"3","firstName":"James","lastName":"Bond"}]}});
+const dataBuffer = Buffer.from(data);
+
+app.get('/publish', (req,res) => {
+	pubSubClient
+        .topic(topicNameOrId)
+        .publish(dataBuffer).then( (messageId) => {
+        	console.log(`Message ${messageId} published.`);
+        	res.status(200).send(`Message ${messageId} published.`);
+        }, (error) => {
+        	console.error(`Received error while publishing: ${error.message}`);
+        	res.status(500).send(`Received error while publishing: ${error.message}`);
+        });
+});
+
+const appPort = process.env.PORT || 5050;
+
+app.listen(appPort, '0.0.0.0', function() {
+  console.log('cat server started, listening on port', appPort);
+});
